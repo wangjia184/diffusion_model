3ee2d3aa89f6a1ecc2cf8701927a78de22119c4f
wangjia184 <vcer@qq.com>
2023-02-26 14:09:47 +0800

    + prev

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -150,7 +150,7 @@
 </script>
 
 <svelte:head>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./bootstrap/dist/css/bootstrap.min.css">
 </svelte:head>
 
 
