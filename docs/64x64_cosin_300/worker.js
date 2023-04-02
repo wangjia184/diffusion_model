cdb7d8b964d025afa00a575334dd0c46d96b1164
wangjia184 <vcer@qq.com>
2023-04-02 17:22:42 +0800

    + prev

--- docs/64x64_cosin_300/worker.js
+++ docs/64x64_cosin_300/worker.js
@@ -2,9 +2,9 @@
 
 const url = self.location.toString();
 let index = url.lastIndexOf('/');
-index = url.lastIndexOf('/', index-1);
+index = url.lastIndexOf('/', index - 1);
 
-const TF_JS_URL = url.substring( 0, index) + "/@tensorflow/tfjs/dist/tf.min.js";
+const TF_JS_URL = url.substring(0, index) + "/@tensorflow/tfjs/dist/tf.min.js";
 const TF_JS_CDN_URL = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.2.0/dist/tf.min.js";
 async function load_model() {
 
@@ -12,8 +12,8 @@ async function load_model() {
         try {
             self.postMessage({ type: 'progress', progress: 0.1, message: 'Loading model' });
             return await tf.loadGraphModel('./model.json', {
-                onProgress : (percent) => {
-                    self.postMessage({ type: 'progress', progress: 0.1 + 0.9*percent, message: 'Loading model' });
+                onProgress: (percent) => {
+                    self.postMessage({ type: 'progress', progress: 0.1 + 0.9 * percent, message: 'Loading model' });
                 }
             });
         }
@@ -29,7 +29,7 @@ async function load_model() {
         return;
     }
 
-    
+
     return model;
 }
 
@@ -43,30 +43,30 @@ async function main() {
         await tf.setBackend('wasm');
         console.log('Successfully loaded WASM backend');
     }
-    
+
 
 
     const model = await load_model();
 
-    const stable_sqrt = (number) => number > 0 && number < 0.001 ? Math.exp( 0.5 * Math.log(number) ) : Math.sqrt(number);
+    const stable_sqrt = (number) => number > 0 && number < 0.001 ? Math.exp(0.5 * Math.log(number)) : Math.sqrt(number);
 
     // cosine schedule as proposed in https://arxiv.org/abs/2102.09672
     const cosine_beta_schedule = (timesteps, s) => {
         const steps = timesteps + 1.0;
-        let alphas_cumprod = tf.linspace(0, timesteps, steps).arraySync().map( x => {
-            return Math.pow( Math.cos(((x / timesteps) + s) / (1 + s) * Math.PI * 0.5), 2);
+        let alphas_cumprod = tf.linspace(0, timesteps, steps).arraySync().map(x => {
+            return Math.pow(Math.cos(((x / timesteps) + s) / (1 + s) * Math.PI * 0.5), 2);
         });
         const base = alphas_cumprod[0];
-        alphas_cumprod = alphas_cumprod.map( x => x / base);
+        alphas_cumprod = alphas_cumprod.map(x => x / base);
         const betas = new Array(timesteps);
-        for( let index = 0; index < betas.length; index++){
-            const b = 1 - (alphas_cumprod[index+1] / alphas_cumprod[index]);
+        for (let index = 0; index < betas.length; index++) {
+            const b = 1 - (alphas_cumprod[index + 1] / alphas_cumprod[index]);
             betas[index] = Math.min(0.9999, Math.max(0.0001, b));
         }
         return betas;
     };
 
-    
+
     const image_size = 64;
     const timesteps = 300;
     //const betas = tf.linspace( beta_start, beta_end, timesteps).arraySync();
@@ -79,39 +79,48 @@ async function main() {
     alphas_cumprod_prev[0] = 1.0;
 
     // prepare variables
-    betas.forEach( (beta, index) => {
+    betas.forEach((beta, index) => {
         alphas[index] = 1.0 - beta;
 
-        alphas_cumprod[index] = (index > 0) ? alphas_cumprod[index-1] * alphas[index] : alphas[index];
+        alphas_cumprod[index] = (index > 0) ? alphas_cumprod[index - 1] * alphas[index] : alphas[index];
 
-        if( index < timesteps - 1 ){
-            alphas_cumprod_prev[index+1] = alphas_cumprod[index];
+        if (index < timesteps - 1) {
+            alphas_cumprod_prev[index + 1] = alphas_cumprod[index];
         }
 
-        stddevs[index] = stable_sqrt( beta * (1.0 - alphas_cumprod_prev[index]) / (1.0 - alphas_cumprod[index]) )
+        stddevs[index] = stable_sqrt(beta * (1.0 - alphas_cumprod_prev[index]) / (1.0 - alphas_cumprod[index]))
     });
 
-    if( typeof(console.log) === 'function' ){
+    if (typeof (console.log) === 'function') {
         console.log("betas=", betas);
         console.log("alphas=", alphas);
         console.log("alphas_cumprod=", alphas_cumprod);
         console.log("alphas_cumprod_prev=", alphas_cumprod_prev);
         console.log("stddevs=", stddevs);
     }
-    
-    self.postMessage({ type: 'ready' });
 
-    const ddpm_p_sample = (image_input, timeStep) => {
+    self.postMessage({
+        type: 'ready',
+        parameters: {
+            betas: betas,
+            alphas: alphas,
+            alphas_cumprod: alphas_cumprod,
+            alphas_cumprod_prev: alphas_cumprod_prev,
+            stddevs: stddevs
+        }
+    });
+
+    const ddpm_p_sample = (xt, timeStep) => {
         // When using WebGL backend, tf.Tensor memory must be managed explicitly (it is not sufficient to let a tf.Tensor go out of scope for its memory to be released).
         // Here we use an array to collect all tensors to be disposed when this method exits
         const collection = new Array();
-        
+
         const time_input = tf.tensor(timeStep, [1]/*shape*/, 'int32' /* model.signature.inputs.time_input.dtype */);
         collection.push(time_input);
 
         const inputs = {
-            time_input : time_input,
-            image_input : image_input
+            time_input: time_input,
+            image_input: xt
         };
 
         const epsilon = model.predict(inputs);
@@ -120,7 +129,7 @@ async function main() {
         const epsilon2 = epsilon.mul(stable_sqrt(1 - alphas_cumprod[timeStep]));
         collection.push(epsilon2);
 
-        const xt_sub_epsilon2 = image_input.sub(epsilon2);
+        const xt_sub_epsilon2 = xt.sub(epsilon2);
         collection.push(xt_sub_epsilon2);
 
         const x0 = xt_sub_epsilon2.div(stable_sqrt(alphas_cumprod[timeStep]));
@@ -130,84 +139,53 @@ async function main() {
         collection.push(clipped_x0);
 
         const x0_coefficient = stable_sqrt(alphas_cumprod_prev[timeStep]) * betas[timeStep] / (1 - alphas_cumprod[timeStep]);
-        const x0_multipled_by_coef = clipped_x0.mul(x0_coefficient); 
+        const x0_multipled_by_coef = clipped_x0.mul(x0_coefficient);
         collection.push(x0_multipled_by_coef);
 
         const xt_coefficient = stable_sqrt(alphas[timeStep]) * (1 - alphas_cumprod_prev[timeStep]) / (1 - alphas_cumprod[timeStep]);
-        const xt_multipled_by_coef = image_input.mul(xt_coefficient);
+        const xt_multipled_by_coef = xt.mul(xt_coefficient);
         collection.push(xt_multipled_by_coef);
 
         const mean = x0_multipled_by_coef.add(xt_multipled_by_coef);
         collection.push(mean);
 
-        let normal_noise = tf.randomNormal(image_input.shape, 0/*mean*/, 1/*stddev*/, 'float32', Math.random()*10000/*seed*/);
+        let normal_noise = tf.randomNormal(xt.shape, 0/*mean*/, 1/*stddev*/, 'float32', Math.random() * 10000/*seed*/);
         collection.push(normal_noise);
 
-        normal_noise = normal_noise.mul(stddevs[timeStep]); 
+        normal_noise = normal_noise.mul(stddevs[timeStep]);
         collection.push(normal_noise);
 
-        const xt_minus_one = mean.add(normal_noise); 
+        const xt_minus_one = mean.add(normal_noise);
 
-        collection.forEach( (t) => t.dispose() );
+        collection.forEach((t) => t.dispose());
         return xt_minus_one;
     };
 
-    const map = {};
-    let keySource = 0;
-    self.onmessage = (evt) => {
-        const request = evt.data;
+    for (; ;) {
+        let timestep = timesteps - 1;
+        const shape = [1, image_size, image_size, 3];
+        let xt = tf.randomNormal(shape, 0/*mean*/, 1/*stddev*/, 'float32', Math.random() * 10000/*seed*/);
+        while (timestep >= 0) {
+            const xt_minus_one = ddpm_p_sample(xt, timestep);
+            xt.dispose();
+            xt = xt_minus_one;
 
-        switch (request.type) {
-            case 'ddpmStart': {
-                const timestep = timesteps - 1;
-                const shape = [1, image_size, image_size, 3];
-                const initial_noises = tf.randomNormal(shape, 0/*mean*/, 1/*stddev*/, 'float32', Math.random()*10000/*seed*/);
-                const image = ddpm_p_sample(initial_noises, timestep);
-                initial_noises.dispose();
-                const key = ++keySource;
-                map[key] = {
-                    step : timestep,
-                    image : image
-                };
-                
-                self.postMessage({ type: 'reply', id: request.id, data: {
-                    step : timestep,
-                    image : image.arraySync(),
-                    key : key,
-                    percent : (timesteps - timestep) / (1.0 * timesteps),
-                } });
-                break;
-            }
-
-            case 'ddpmNext': {
-                const prev = map[request.kwargs && request.kwargs.key];
-                if(!prev) {
-                    console.log('invalid request');
-                } else {
-                    delete map[request.kwargs.key];
-                    const timestep = prev.step - 1;
-                    const image = ddpm_p_sample(prev.image, timestep);
-                    prev.image.dispose();
-                    if(timestep > 0) {
-                        map[request.kwargs.key] = {
-                            step : timestep,
-                            image : image
-                        };
-                    }
-                    self.postMessage({ type: 'reply', id: request.id, data: {
-                        step : timestep,
-                        image : image.arraySync(),
-                        key : request.kwargs.key,
-                        percent : (timesteps - timestep) / (1.0 * timesteps)
-                    } });
-                }
-                break;
-            }
-    
+            self.postMessage({
+                type: 'image',
+                timestep: timestep,
+                image: xt_minus_one.arraySync(),
+                percent: (timesteps - timestep) / (1.0 * timesteps)
+            });
+            timestep--;
         }
-    };
+        xt.dispose();
+    }
 
-    
+
+
+
+
+
 }
 
 
@@ -215,16 +193,16 @@ self.postMessage({ type: 'progress', progress: 0, message: 'Loading ' + TF_JS_UR
 import(TF_JS_URL)
     .then(async () => {
 
-       await main();
+        await main();
 
     })
     .catch(async (err) => {
 
-        try{
+        try {
             await import(TF_JS_CDN_URL);
             await main();
         }
-        catch{
+        catch {
             self.postMessage({ type: 'error', message: 'Unable to load ' + TF_JS_URL });
             console.log('Unable to load ' + TF_JS_URL, err);
         }
--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,31 +1,48 @@
 <script>
-  import { Progress } from 'sveltestrap';
-  import { onMount } from 'svelte';
+  import {
+    Progress,
+    Table,
+    Modal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+    Button,
+  } from "sveltestrap";
+  import { onMount } from "svelte";
+  import t_jpg from "./assets/t.jpg";
+  import beta_t_jpg from "./assets/beta_t.jpg";
+  import alpha_t_jpg from "./assets/alpha_t.jpg";
+  import alphas_cumprod_jpg from "./assets/alphas_cumprod.jpg";
+  import alphas_cumprod_prev_jpg from "./assets/alphas_cumprod_prev.jpg";
+  import sigma_jpg from "./assets/sigma.jpg";
 
   let loadingPercentage = 0;
   let canvas;
-  let idSource = 0;
   let percentage = 0;
   let images = [];
-	const commandMap = {};
-  const worker = new Worker("./64x64_cosin_300/worker.js");
+  let parameters = null;
+  let isModelOpen = false;
+  const worker = new Worker("./64x64_cosin_300/worker.js?2");
 
-  onMount( () => {
+  onMount(() => {
     worker.onmessage = async (evt) => {
       const data = evt.data;
       switch (data.type) {
-        case "reply": {
-          const handler = commandMap[data.id];
-          if (handler) {
-            handler.resolve(data.data);
-          } else {
-            console.log("Unable to find the promise.", data);
+        case "image": {
+          if (!isModelOpen || data.timestep == 0) {
+            updateCanvas(data.image);
+          }
+
+          percentage = data.percent;
+          if (data.timestep == 0) {
+            images.push(canvas.toDataURL());
+            images = images;
           }
           break;
         }
         case "ready": {
           loadingPercentage = 100;
-          setTimeout( () => ddpmGenerate());
+          parameters = data.parameters;
           break;
         }
         case "progress": {
@@ -44,66 +61,6 @@
       }
     };
   });
-  
-
-  const registerPromise = () => {
-		const id = idSource++;
-		const promise = new Promise((resolve, reject) => {
-			commandMap[id] = {
-				resolve: resolve,
-				reject: reject,
-			};
-		});
-		return { id: id, promise: promise };
-	};
-
-  const ddpmStart = async () => {
-		const { id, promise } = registerPromise();
-		const msg = {
-			type: "ddpmStart",
-			id: id,
-			kwargs: {
-			},
-		};
-		worker.postMessage(msg);
-		return await promise;
-	};
-
-  const ddpmNext = async (key) => {
-		const { id, promise } = registerPromise();
-		const msg = {
-			type: "ddpmNext",
-			id: id,
-			kwargs: {
-        key : key
-			},
-		};
-		worker.postMessage(msg);
-		return await promise;
-	};
-
-  const ddpmGenerate = async() => {
-    percentage = 0;
-    if(canvas && canvas.width && canvas.height) {
-      const ctx = canvas.getContext("2d");
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-    
-    let response = await ddpmStart();
-    updateCanvas(response.image);
-    percentage = response.percent;
-
-    while(response.step > 0){
-      response = await ddpmNext(response.key);
-      updateCanvas(response.image);
-      percentage = response.percent;
-    }
-
-    images.push(canvas.toDataURL());
-    images = images;
-
-    await ddpmGenerate();
-  };
 
   const scale = 2;
   const updateCanvas = (img) => {
@@ -113,79 +70,134 @@
     canvas.width = width * scale;
     canvas.height = height * scale;
     const ctx = canvas.getContext("2d");
-    const id = ctx.createImageData(width * scale, height * scale, { colorSpace : 'srgb'});
+    const id = ctx.createImageData(width * scale, height * scale, {
+      colorSpace: "srgb",
+    });
     const pixels = id.data;
 
-    for( let y = 0; y < height; y ++){
-        for( let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const channels = img[0][y][x];
 
-          const channels = img[0][y][x];
+        const r = Math.max(0, Math.min(255, channels[0] * 127.5 + 127.5));
+        const g = Math.max(0, Math.min(255, channels[1] * 127.5 + 127.5));
+        const b = Math.max(0, Math.min(255, channels[2] * 127.5 + 127.5));
 
-          const r = Math.max( 0, Math.min( 255, channels[0] * 127.5 + 127.5));
-          const g = Math.max( 0, Math.min( 255, channels[1] * 127.5 + 127.5));
-          const b = Math.max( 0, Math.min( 255, channels[2] * 127.5 + 127.5));
+        for (let offsetX = 0; offsetX < scale; offsetX++) {
+          for (let offsetY = 0; offsetY < scale; offsetY++) {
+            const offset =
+              ((y * scale + offsetY) * width * scale + x * scale + offsetX) * 4;
 
-          for( let offsetX = 0; offsetX < scale; offsetX++){
-            for( let offsetY = 0; offsetY < scale; offsetY++){
-
-              const offset = ( (y*scale+offsetY) * width * scale + x*scale + offsetX ) * 4;
-           
-
-            
-              pixels[offset] = r;
-              pixels[offset + 1] = g;
-              pixels[offset + 2] = b;
-              pixels[offset + 3] = 255;
-            }
-            
+            pixels[offset] = r;
+            pixels[offset + 1] = g;
+            pixels[offset + 2] = b;
+            pixels[offset + 3] = 255;
           }
         }
-    }                  
+      }
+    }
 
     ctx.putImageData(id, 0, 0);
-
-  }
-
-
+  };
 </script>
 
 <svelte:head>
-  <link rel="stylesheet" href="./bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./bootstrap/dist/css/bootstrap.min.css" />
 </svelte:head>
 
-
-
-{#if loadingPercentage < 100 }
-<div>
-  <div class="text-center">Loading ...</div>
-  <Progress animated color="success" value={loadingPercentage}>{loadingPercentage}%</Progress>
-</div>
+{#if loadingPercentage < 100}
+  <div>
+    <div class="text-center">Loading ...</div>
+    <Progress animated color="success" value={loadingPercentage}
+      >{loadingPercentage}%</Progress
+    >
+  </div>
 {:else}
-
-
-<div class="container">
-  <div class="row">
-    {#each images as img }
-    <div class="col">
-      <img src={img} class="generated mb-2" alt=""/>
-    </div>
-    
-    {/each}
-    <div class="col">
-      <div class="d-flex justify-content-center">
-        <div >
-          <canvas class="d-flex" bind:this={canvas}></canvas>
-          <div class="d-flex progress_bar" style="width:{percentage*100}%"></div>
+  <div class="container">
+    <div class="row">
+      {#each images as img}
+        <div class="col">
+          <img src={img} class="generated mb-2" alt="" />
+        </div>
+      {/each}
+      <div class="col">
+        <div class="d-flex justify-content-center">
+          <div>
+            <canvas class="d-flex" bind:this={canvas} />
+            <div
+              class="d-flex progress_bar"
+              style="width:{percentage * 100}%"
+            />
+            {#if parameters}
+              <Button
+                size="sm"
+                color="secondary"
+                on:click={() => (isModelOpen = true)}>Show Variables</Button
+              >
+            {/if}
+          </div>
         </div>
       </div>
     </div>
   </div>
-
-</div>
 {/if}
 
-
-
+<Modal
+  isOpen={isModelOpen}
+  toggle={() => (isModelOpen = false)}
+  fullscreen={true}
+>
+  <ModalHeader toggle={() => (isModelOpen = false)}>Variables</ModalHeader>
+  <ModalBody>
+    <Table bordered>
+      <thead style="position:sticky; top: 0;">
+        <tr>
+          <th style="background-color:white"
+            ><img alt="timestep" src={t_jpg} /></th
+          >
+          <th style="background-color:white"
+            ><img alt="beta t" src={beta_t_jpg} /></th
+          >
+          <th style="background-color:white"
+            ><img alt="alpha t" src={alpha_t_jpg} /></th
+          >
+          <th style="background-color:white"
+            ><img alt="alpha bar t" src={alphas_cumprod_jpg} /></th
+          >
+          <th style="background-color:white"
+            ><img
+              alt="alpha bar t minus one"
+              src={alphas_cumprod_prev_jpg}
+            /></th
+          >
+          <th style="background-color:white"
+            ><img alt="std dev" src={sigma_jpg} /></th
+          >
+        </tr>
+      </thead>
+      <tbody>
+        {#each parameters.betas as beta, index}
+          <tr>
+            <th scope="row">{index + 1}</th>
+            <td><code>{beta}</code></td>
+            <td><code>{parameters.alphas[index].toFixed(19)}</code></td>
+            <td><code>{parameters.alphas_cumprod[index].toFixed(19)}</code></td>
+            <td
+              ><code>{parameters.alphas_cumprod_prev[index].toFixed(19)}</code
+              ></td
+            >
+            <td><code>{parameters.stddevs[index].toFixed(19)}</code></td>
+          </tr>
+        {/each}
+      </tbody>
+    </Table>
+  </ModalBody>
+  <ModalFooter>
+    <Button color="secondary" on:click={() => (isModelOpen = false)}
+      >Close</Button
+    >
+  </ModalFooter>
+</Modal>
 
 <style>
   .progress_bar {
@@ -194,14 +206,14 @@
   }
 
   .generated {
-    width:128px;
-    height:128px;
+    width: 128px;
+    height: 128px;
     border: solid 1px #666;
   }
 
   :global(canvas) {
-    width:128px;
-    height:128px;
+    width: 128px;
+    height: 128px;
     border: solid 1px #666;
   }
 </style>
